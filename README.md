6934a3405e9183d71d8b99c7f0742032c00159ec
Olena Kulichenko <Kulichenko@axinom.com>
2017-12-13 11:13:16 +0200

    app

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# eeg-reading.github.io
-Adjusting web-pages to the level of the user's attention
+# Muse Blink
+
+Simple blink detection app using [Muse EEG Headset](https://choosemuse.com/) and [Angular](https://angular.io)
+
+## Development server
+
+Run `npm start` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Build
+
+Run `npm run build` to build the project. The build artifacts will be stored in the `dist/` directory.
+
+## Running unit tests
+
+Run `npm test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { MuseClient, channelNames } from 'muse-js';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  private muse = new MuseClient();
+  connected = false;
+  leftBlinks: Observable<number>;
+  rightBlinks: Observable<number>;
+
+  constructor() {
+    this.muse.connectionStatus.subscribe(newStatus => {
+      this.connected = newStatus;
+    });
+  }
+
+  async onConnectButtonClick() {
+    await this.muse.connect();
+    this.muse.start();
+
+    const leftEyeChannel = channelNames.indexOf('AF7');
+    const rightEyeChannel = channelNames.indexOf('AF8');
+
+    this.leftBlinks = this.muse.eegReadings
+      .filter(r => r.electrode === leftEyeChannel)
+      .map(r => Math.max(...r.samples.map(n => Math.abs(n))))
+      .filter(max => max > 500)
+      .switchMap(() =>
+        Observable.merge(
+          Observable.of(1),
+          Observable.timer(500).map(() => 0)
+        )
+      );
+
+    this.rightBlinks = this.muse.eegReadings
+      .filter(r => r.electrode === rightEyeChannel)
+      .map(r => Math.max(...r.samples.map(n => Math.abs(n))))
+      .filter(max => max > 500)
+      .switchMap(() =>
+        Observable.merge(
+          Observable.of(1),
+          Observable.timer(500).map(() => 0)
+        )
+      );
+
+    this.leftBlinks.subscribe(value => {
+      console.log('Blink!', value);
+    });
+  }
+
+  disconnect() {
+    this.muse.disconnect();
+  }
+}
